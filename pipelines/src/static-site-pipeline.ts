e22988d47c72c62b011cc062a1bc9fc90b778a60
Dinesh Ogirala <dinesh.ogirala@peopletech.com>
2023-09-10 12:27:33 -0700

    changed build image version to 5.0

--- pipelines/src/static-site-pipeline.ts
+++ pipelines/src/static-site-pipeline.ts
@@ -121,7 +121,7 @@ class TriviaGameStaticSitePipeline extends Stack {
         const project = new codebuild.PipelineProject(this, stageName + 'Project', {
             buildSpec: codebuild.BuildSpec.fromSourceFilename('static-site/buildspec.yml'),
             environment: {
-                buildImage: codebuild.LinuxBuildImage.fromCodeBuildImageId('aws/codebuild/amazonlinux2-x86_64-standard:4.0'),
+                buildImage: codebuild.LinuxBuildImage.fromCodeBuildImageId('aws/codebuild/amazonlinux2-x86_64-standard:5.0'),
                 environmentVariables: {
                     'STAGE': {
                         value: stageName.toLowerCase()
